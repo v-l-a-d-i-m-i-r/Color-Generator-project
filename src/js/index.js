bb9e21f053b989cea89e46f80ed9dc71d1a6a183
AnnaGorbachev <anna.gorbacheva231@gmail.com>
2022-11-22 13:53:44 +0200

    feat(4): palette calculator

--- src/js/index.js
+++ src/js/index.js
@@ -1,39 +1,113 @@
 
+const hex_start = document.querySelector('#hex-start');
+const res_hex_start = document.querySelector('#res_hex-start');
 
-// const inputs = document.querySelectorAll('.input');
-// console.log(inputs[1]);
-// console.log(inputs[2]);
-
-// const inputColor = inputs[1];
-// const inputNumber = inputs[2];
-
-const hex_one = document.querySelector('#hex-one');
-const res_hex_one = document.querySelector('#res_hex-one');
-
-const hex_two = document.querySelector('#hex-two');
-const res_hex_two = document.querySelector('#res_hex-two');
+const hex_finish = document.querySelector('#hex-finish');
+const res_hex_finish = document.querySelector('#res_hex-finish');
 
 const qtg = document.querySelector('#qtg');
 const res_qtg = document.querySelector('#res_qtg');
 
-hex_one.addEventListener("input", function () {
+function handler() {
 
-  res_hex_one.innerHTML = hex_one.value;
+    const palette = paletteCalculator(hex_start.value, hex_finish.value, qtg.value)
+    console.log(palette);
+}
 
-});
+hex_start.addEventListener("input", handler);
+hex_finish.addEventListener("input", handler);
+qtg.addEventListener("input", handler);
+
+function chunksHex(hex) {
+    let str = hex.slice(1);
+    let arr = [];
+    while (str !== '') {
+
+        let num = parseInt(str.slice(0, 2), 16);
+
+        arr.push(num);
+        str = str.slice(2);
+    }
+    return arr;
+}
+
+const arrStart = chunksHex("#f5cccc");
+const arrFinish = chunksHex("#c52626");
+
+function paletteCalculator(color1, color2, num) {
+
+    const [r1, g1, b1] = chunksHex(color1);
+    const [r2, g2, b2] = chunksHex(color2);
+
+    const stepR = (r2 - r1) / (num - 1);
+    const stepG = (g2 - g1) / (num - 1);
+    const stepB = (b2 - b1) / (num - 1);
+
+    let arr = [];
+    for (i = 0; i < num; i++) {
+
+        const resR = Math.round(r1 + i * stepR);
+        const resG = Math.round(g1 + i * stepG);
+        const resB = Math.round(b1 + i * stepB);
+        arr.push(rgbToHex(resR, resG, resB));
+
+    }
+    return arr;
+}
+
+// console.log(paletteCalculator('#ffffff', '#000000', 4));
 
 
-hex_two.addEventListener("input", function () {
 
-  res_hex_two.innerHTML = hex_two.value;
+// function hexToRgb(hex) {
+//   let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+//   return result ? {
+//     r: parseInt(result[1], 16),
+//     g: parseInt(result[2], 16),
+//     b: parseInt(result[3], 16)
+//   } : null;
+// }
 
-});
+// console.log(hexToRgb("#0033ff").g); // "51";
 
-qtg.addEventListener("input", function () {
 
-  res_qtg.innerHTML = qtg.value;
+function componentToHex(c) {
+    let hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+};
 
-});
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
